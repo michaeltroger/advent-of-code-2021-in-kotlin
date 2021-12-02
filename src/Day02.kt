f2dc9d5ed047ac58681fa00c21d30cbd574b727f
Michael Troger <dev@michaeltroger.com>
2021-12-02 18:17:48 +0100

    Solve part 2 day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -8,7 +8,7 @@ private class Command(val direction: Direction, val increment: Int)
 
 private fun Command(input: String): Command {
     val inputs = input.split(" ")
-    return Command(Direction.valueOf(inputs[0].toUpperCase()), inputs[1].toInt())
+    return Command(Direction.valueOf(inputs[0].uppercase()), inputs[1].toInt())
 }
 
 fun main() {
@@ -26,13 +26,30 @@ fun main() {
     }
 
     fun part2(input: List<Command>): Long {
-        return 3L
+        var horizontal = 0
+        var vertical = 0
+        var aim = 0
+        input.forEach {
+            when (it.direction) {
+                Direction.FORWARD -> {
+                    vertical += aim * it.increment
+                    horizontal += it.increment
+                }
+                Direction.DOWN -> {
+                    aim +=  it.increment
+                }
+                Direction.UP -> {
+                    aim -=  it.increment
+                }
+            }
+        }
+        return horizontal.toLong() * vertical
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test").map { Command(it) }
     check(part1(testInput) == 150L)
-//    check(part2(testInput) == 150L)
+    check(part2(testInput) == 900L)
 
     val input = readInput("Day02").map { Command(it) }
     println(part1(input))
