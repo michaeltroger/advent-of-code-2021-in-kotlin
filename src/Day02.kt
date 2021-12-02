92e5f512ebb853f43f6b2af83ad5d5dcac5522c0
Michael Troger <dev@michaeltroger.com>
2021-12-02 21:19:59 +0100

    Refactor

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,16 +1,3 @@
-private enum class Direction {
-    FORWARD,
-    DOWN,
-    UP,
-}
-
-private class Command(val direction: Direction, val increment: Int)
-
-private fun Command(input: String): Command {
-    val inputs = input.split(" ")
-    return Command(Direction.valueOf(inputs[0].uppercase()), inputs[1].toInt())
-}
-
 fun main() {
     fun part1(input: List<Command>): Long {
         var horizontal = 0
@@ -50,4 +37,17 @@ fun main() {
     val input = readInput("Day02").map { Command(it) }
     println(part1(input))
     println(part2(input))
+}
+
+private enum class Direction {
+    FORWARD,
+    DOWN,
+    UP,
+}
+
+private class Command(val direction: Direction, val increment: Int)
+
+private fun Command(input: String): Command {
+    val inputs = input.split(" ")
+    return Command(Direction.valueOf(inputs[0].uppercase()), inputs[1].toInt())
 }
