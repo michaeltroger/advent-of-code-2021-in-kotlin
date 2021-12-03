f841dfb157f9626b0f1e8e8e9991f10357eb1198
Michael Troger <michael.troger@runtastic.com>
2021-12-03 10:47:20 +0100

    Reuse code

--- src/Day3.kt
+++ src/Day3.kt
@@ -2,8 +2,14 @@ private const val DAY = 3
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val (gammaRateInBit, epsilonRateInBit) = findMostAndLeastCommonBits(input)
-        return gammaRateInBit.concatToString().toInt(2) * epsilonRateInBit.concatToString().toInt(2)
+        var mostCommonBits = ""
+        var leastCommonBits = ""
+        for (i in input[0].indices) {
+            val (mostCommonBit, leastCommonBit) = findMostAndLeastCommonBit(input, i)
+            mostCommonBits += mostCommonBit
+            leastCommonBits += leastCommonBit
+        }
+        return mostCommonBits.toInt(2) * leastCommonBits.toInt(2)
     }
 
     fun part2(input: List<String>): Int {
@@ -27,55 +33,22 @@ private fun findRemainingBitNumberRecursively(input: List<String>, searchMostCom
         return input.first()
     }
 
-    val (mostCommonBit, leastCommonBit) = findMostAndLeastCommonBitAtIndex(input, index)
+    val (mostCommonBit, leastCommonBit) = findMostAndLeastCommonBit(input, index)
     val bitToCompare = if (searchMostCommon) {
         mostCommonBit
     } else {
         leastCommonBit
     }
-    val newList = mutableListOf<String>()
-    input.forEach { bitNumber: String ->
-        if (bitNumber[index] == bitToCompare) {
-            newList.add(bitNumber)
-        }
+    val newList = input.filter {
+        it[index] == bitToCompare
     }
     return findRemainingBitNumberRecursively(newList, searchMostCommon, index + 1)
 }
 
-private fun findMostAndLeastCommonBits(input: List<String>): Pair<CharArray, CharArray> {
-    val countedZeros = IntArray(input[0].length)
-    val countedOnes = IntArray(input[0].length)
-    for ((x, line) in input.withIndex()) {
-        for ((y, _) in line.withIndex()) {
-            when (input[x][y]) {
-                '0' -> countedZeros[y]++
-                '1' -> countedOnes[y]++
-            }
-        }
-    }
-
-    val mostCommonBits = CharArray(countedZeros.size)
-    val leastCommonBits = CharArray(countedZeros.size)
-    for (i in countedZeros.indices) {
-        val mostCommonBit: Char
-        val leastCommonBit: Char
-        if (countedOnes[i] >= countedZeros[i]) {
-            mostCommonBit = '1'
-            leastCommonBit = '0'
-        } else {
-            mostCommonBit = '0'
-            leastCommonBit = '1'
-        }
-        mostCommonBits[i] = mostCommonBit
-        leastCommonBits[i] = leastCommonBit
-    }
-    return mostCommonBits to leastCommonBits
-}
-
-private fun findMostAndLeastCommonBitAtIndex(input: List<String>, index: Int): Pair<Char, Char> {
+private fun findMostAndLeastCommonBit(input: List<String>, index: Int): Pair<Char, Char> {
     var countedZeros = 0
     var countedOnes = 0
-    for ((x, _) in input.withIndex()) {
+    for (x in input.indices) {
         when (input[x][index]) {
             '0' -> countedZeros++
             '1' -> countedOnes++
