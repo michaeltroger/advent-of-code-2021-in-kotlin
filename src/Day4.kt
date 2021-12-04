fc3a4b68f098a9a0ed7386abe0647dd01937a22c
Michael Troger <dev@michaeltroger.com>
2021-12-04 14:36:30 +0100

    Solve part 1 day 4

--- src/Day4.kt
+++ src/Day4.kt
@@ -2,9 +2,30 @@ private const val DAY = 4
 
 fun main() {
     fun part1(input: Input): Int {
-        println(input.randomNumbers.size)
-        println(input.bingoBoards.size)
-        return 0
+        val randomNumbers: List<Byte> = input.randomNumbers
+        val boards: List<Board> = input.bingoBoards
+        randomNumbers.forEach { drawnNumber ->
+            // mark fields
+            boards.forEach { board ->
+                board.numbers.forEach { row ->
+                    row.filter { it.number == drawnNumber }.forEach { field -> field.marked = true }
+                }
+            }
+
+            // check if row/column complete
+            boards.forEach { board ->
+                board.numbers.forEachIndexed { x, rows ->
+                    val totalMarkedInX = rows.count { it.marked }
+                    val totalMarkedInY = board.numbers.mapIndexed { _, list -> list[x] }.count { it.marked }
+                    if (totalMarkedInX == 5 || totalMarkedInY == 5) {
+                        val sum = board.numbers.flatten().filter { !it.marked }.map { it.number }.sum()
+                        return sum * drawnNumber
+                    }
+                }
+            }
+        }
+
+        return -1
     }
 
     fun part2(input: Input): Int {
@@ -16,11 +37,11 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = toInput(readInput(day = DAY, useTestInput = true))
 
-    check(part1(testInput) == 198)
-    check(part2(testInput) == 230)
+    check(part1(testInput) == 4512)
+    //check(part2(testInput) == 230)
 
     val input = toInput(readInput(day = DAY))
-    //println(part1(input))
+    println(part1(input))
     //println(part2(input))
 }
 
@@ -39,11 +60,14 @@ private fun toInput(text: List<String>): Input {
             it.replace("  ", " ")
         }.map { row ->
             row.split(" ").map {
-                it.toByte()
+                Field(it.toByte())
             }
-        }.chunked(5)
-        .toList()
+        }.chunked(5).map {
+            Board(it)
+        }.toList()
     return Input(randomNumbers, bingoBoards)
 }
 
-private class Input(val randomNumbers: List<Byte>, val bingoBoards: List<List<List<Byte>>>)
+private class Input(val randomNumbers: List<Byte>, val bingoBoards: List<Board>)
+private class Field(val number: Byte, var marked: Boolean = false)
+private class Board(val numbers: List<List<Field>>)
