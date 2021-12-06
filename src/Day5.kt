376ab1e9020706b91f2fb7fabae2ada3ec50124a
Michael Troger <dev@michaeltroger.com>
2021-12-06 21:13:36 +0100

    Refactor

--- src/Day5.kt
+++ src/Day5.kt
@@ -3,87 +3,36 @@ private const val DAY = 5
 fun main() {
     fun part1(input: List<Line>): Int {
         val max = input.getMax()
+        val matrix = Array(max + 1) { IntArray(max + 1) }
 
-        val array = Array(max + 1) { IntArray(max + 1) }
         input.forEach { line ->
-            var (x1, y1) = line.from
-            var (x2, y2) = line.to
-            if (x1 == x2) {
-                if (y1 > y2) {
-                    val tempY = y1
-                    y1 = y2
-                    y2 = tempY
-                }
-                for (y in y1 until y2 + 1) {
-                    array[y][x1]++
-                }
-            } else if (y1 == y2) {
-                if (x1 > x2) {
-                    val tempX = x1
-                    x1 = x2
-                    x2 = tempX
-                }
-                for (x in x1 until x2 + 1) {
-                    array[y1][x]++
-                }
+            val (x1, y1) = line.from
+            val (x2, y2) = line.to
+            when {
+                x1 == x2 -> matrix.markDangerZoneXEqual(line)
+                y1 == y2 -> matrix.markDangerZoneYEqual(line)
             }
         }
 
-        return array.toList().map { it.toList() }.flatten().count { it >= 2 }
+        return getDangerZoneCount(matrix)
     }
 
     fun part2(input: List<Line>): Int {
         val max = input.getMax()
+        val matrix = Array(max + 1) { IntArray(max + 1) }
 
-        val array = Array(max + 1) { IntArray(max + 1) }
         input.forEach { line ->
-            var (x1, y1) = line.from
-            var (x2, y2) = line.to
-            if (x1 == x2) {
-                if (y1 > y2) {
-                    val tempY = y1
-                    y1 = y2
-                    y2 = tempY
-                }
-                for (y in y1 until y2 + 1) {
-                    array[y][x1]++
-                }
-            } else if (y1 == y2) {
-                if (x1 > x2) {
-                    val tempX = x1
-                    x1 = x2
-                    x2 = tempX
-                }
-                for (x in x1 until x2 + 1) {
-                    array[y1][x]++
-                }
-            } else {
-                val deltaX = x2 - x1
-                val deltaY = y2 - y1
-
-                val incrementX = deltaX >= 0
-                val incrementY = deltaY >= 0
-
-                var x = x1
-                var y = y1
-                for (i in 0..kotlin.math.abs(deltaX)) {
-                    array[y][x]++
-                    if (incrementX) {
-                        x++
-                    } else {
-                        x--
-                    }
-                    if (incrementY) {
-                        y++
-                    } else {
-                        y--
-                    }
-                }
+            val (x1, y1) = line.from
+            val (x2, y2) = line.to
+            when {
+                x1 == x2 -> matrix.markDangerZoneXEqual(line)
+                y1 == y2 -> matrix.markDangerZoneYEqual(line)
+                else -> matrix.markDangerZoneDiagonal(line)
             }
 
         }
 
-        return array.toList().map { it.toList() }.flatten().count { it >= 2 }
+        return getDangerZoneCount(matrix)
     }
 
     // test if implementation meets criteria from the description, like:
@@ -97,6 +46,62 @@ fun main() {
     println(part2(input))
 }
 
+private fun Array<IntArray>.markDangerZoneXEqual(line: Line) {
+    var (x1, y1) = line.from
+    var y2 = line.to.second
+
+    if (y1 > y2) {
+        val tempY = y1
+        y1 = y2
+        y2 = tempY
+    }
+    for (y in y1 until y2 + 1) {
+        this[y][x1]++
+    }
+}
+
+private fun Array<IntArray>.markDangerZoneYEqual(line: Line) {
+    var (x1, y1) = line.from
+    var x2 = line.to.first
+
+    if (x1 > x2) {
+        val tempX = x1
+        x1 = x2
+        x2 = tempX
+    }
+    for (x in x1 until x2 + 1) {
+        this[y1][x]++
+    }
+}
+
+private fun Array<IntArray>.markDangerZoneDiagonal(line: Line) {
+    val (x1, y1) = line.from
+    val (x2, y2) = line.to
+
+    val deltaX = x2 - x1
+    val deltaY = y2 - y1
+    val incrementX = deltaX >= 0
+    val incrementY = deltaY >= 0
+
+    var x = x1
+    var y = y1
+    repeat(kotlin.math.abs(deltaX) + 1) {
+        this[y][x]++
+        if (incrementX) {
+            x++
+        } else {
+            x--
+        }
+        if (incrementY) {
+            y++
+        } else {
+            y--
+        }
+    }
+}
+
+private fun getDangerZoneCount(matrix: Array<IntArray>) = matrix.toList().map { it.toList() }.flatten().count { it >= 2 }
+
 private class Line(val from: Pair<Int, Int>, val to: Pair<Int, Int>)
 
 private fun Line(text: String): Line {
@@ -104,21 +109,7 @@ private fun Line(text: String): Line {
     return Line(from = line[0] to line[1], to = line[2] to line[3])
 }
 
-private fun List<Line>.getMax(): Int {
-    var max = 0
-    forEach {
-        if (it.to.first > max) {
-            max = it.to.first
-        }
-        if (it.from.first > max) {
-            max = it.from.first
-        }
-        if (it.to.second > max) {
-            max = it.to.second
-        }
-        if (it.from.second > max) {
-            max = it.from.second
-        }
-    }
-    return max
-}
+private fun List<Line>.getMax() =
+    map {
+        maxOf(it.to.first, it.from.first, it.to.second, it.from.second)
+    }.maxOf { it }
