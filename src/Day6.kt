2cfd67a56314113abe637b24ee6fdf3edf85f6fa
Michael Troger <dev@michaeltroger.com>
2021-12-06 21:56:36 +0100

    Solve part 1 day 6

--- src/Day6.kt
+++ src/Day6.kt
@@ -1,8 +1,25 @@
 private const val DAY = 6
 
+private const val DAYS_TO_SIMULATE = 80
+
 fun main() {
     fun part1(input: List<Byte>): Int {
-        return -1
+        val fish = input.toMutableList()
+
+        repeat(DAYS_TO_SIMULATE) {
+            val iterate = fish.listIterator()
+            while (iterate.hasNext()) {
+                when (val internalTimer = iterate.next()) {
+                    0.toByte() -> {
+                        iterate.set(6)
+                        iterate.add(8)
+                    } else -> iterate.set((internalTimer - 1).toByte())
+                }
+
+            }
+        }
+
+        return fish.size
     }
 
     fun part2(input: List<Byte>): Int {
@@ -12,11 +29,11 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = parseInput(readInput(day = DAY, useTestInput = true))
     check(part1(testInput) == 5934)
-    check(part2(testInput) == 5)
+    //check(part2(testInput) == 5)
 
     val input = parseInput(readInput(day = DAY))
     println(part1(input))
-    println(part2(input))
+   // println(part2(input))
 }
 
 private fun parseInput(input: List<String>) = input.first().split(",").map { it.toByte() }
