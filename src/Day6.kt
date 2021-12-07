cb1d64789ec1bbfa18412971dd7cc3bfe4e7a152
Michael Troger <dev@michaeltroger.com>
2021-12-07 18:17:56 +0100

    Refactor

--- src/Day6.kt
+++ src/Day6.kt
@@ -3,9 +3,7 @@ private const val DAY = 6
 fun main() {
     fun getFishPopulationCount(initialState: List<Byte>, daysToSimulate: Int): Long {
         var fishCountPerDay = LongArray(9)
-        initialState.forEach {
-            fishCountPerDay[it.toInt()]++
-        }
+        initialState.forEach { fishCountPerDay[it.toInt()]++ }
         repeat(daysToSimulate) {
             val tempFishCount = LongArray(9)
             fishCountPerDay.forEachIndexed { daysUntilBirth, fishCount ->
