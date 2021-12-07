5040aa16cccbf19eb4adaa6dff02d3ee9dc0b70e
Michael Troger <dev@michaeltroger.com>
2021-12-07 18:10:44 +0100

    Solve part 2 day 6

--- src/Day6.kt
+++ src/Day6.kt
@@ -22,18 +22,38 @@ fun main() {
         return fish.size
     }
 
-    fun part2(input: List<Byte>): Int {
-        return -1
+    fun part2(input: List<Byte>): Long {
+        var fishCountOnDay = LongArray(9)
+        input.forEach {
+            fishCountOnDay[it.toInt()]++
+        }
+        repeat(256) {
+            val tempFishCount = LongArray(9)
+            fishCountOnDay.forEachIndexed { daysUntilBirth, fishCount ->
+                when(daysUntilBirth) {
+                    0 -> {
+                        tempFishCount[6] += fishCount
+                        tempFishCount[8] += fishCount
+                    }
+                    else -> {
+                        tempFishCount[daysUntilBirth - 1] += fishCount
+                    }
+                }
+            }
+            fishCountOnDay = tempFishCount
+
+        }
+        return fishCountOnDay.toList().sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = parseInput(readInput(day = DAY, useTestInput = true))
     check(part1(testInput) == 5934)
-    //check(part2(testInput) == 5)
+    check(part2(testInput) == 26984457539)
 
     val input = parseInput(readInput(day = DAY))
     println(part1(input))
-   // println(part2(input))
+    println(part2(input))
 }
 
 private fun parseInput(input: List<String>) = input.first().split(",").map { it.toByte() }
