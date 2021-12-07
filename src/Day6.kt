23ca117afcfe93bc1563d3a0bd227584dc7ad592
Michael Troger <dev@michaeltroger.com>
2021-12-07 18:17:13 +0100

    Refactor

--- src/Day6.kt
+++ src/Day6.kt
@@ -1,54 +1,33 @@
 private const val DAY = 6
 
-private const val DAYS_TO_SIMULATE = 80
-
 fun main() {
-    fun part1(input: List<Byte>): Int {
-        val fish = input.toMutableList()
-
-        repeat(DAYS_TO_SIMULATE) {
-            val iterate = fish.listIterator()
-            while (iterate.hasNext()) {
-                when (val internalTimer = iterate.next()) {
-                    0.toByte() -> {
-                        iterate.set(6)
-                        iterate.add(8)
-                    } else -> iterate.set((internalTimer - 1).toByte())
-                }
-
-            }
+    fun getFishPopulationCount(initialState: List<Byte>, daysToSimulate: Int): Long {
+        var fishCountPerDay = LongArray(9)
+        initialState.forEach {
+            fishCountPerDay[it.toInt()]++
         }
-
-        return fish.size
-    }
-
-    fun part2(input: List<Byte>): Long {
-        var fishCountOnDay = LongArray(9)
-        input.forEach {
-            fishCountOnDay[it.toInt()]++
-        }
-        repeat(256) {
+        repeat(daysToSimulate) {
             val tempFishCount = LongArray(9)
-            fishCountOnDay.forEachIndexed { daysUntilBirth, fishCount ->
+            fishCountPerDay.forEachIndexed { daysUntilBirth, fishCount ->
                 when(daysUntilBirth) {
                     0 -> {
                         tempFishCount[6] += fishCount
                         tempFishCount[8] += fishCount
                     }
-                    else -> {
-                        tempFishCount[daysUntilBirth - 1] += fishCount
-                    }
+                    else -> tempFishCount[daysUntilBirth - 1] += fishCount
                 }
             }
-            fishCountOnDay = tempFishCount
-
+            fishCountPerDay = tempFishCount
         }
-        return fishCountOnDay.toList().sum()
+        return fishCountPerDay.sum()
     }
+
+    fun part1(input: List<Byte>) = getFishPopulationCount(input, 80)
+    fun part2(input: List<Byte>) = getFishPopulationCount(input, 256)
 
     // test if implementation meets criteria from the description, like:
     val testInput = parseInput(readInput(day = DAY, useTestInput = true))
-    check(part1(testInput) == 5934)
+    check(part1(testInput) == 5934L)
     check(part2(testInput) == 26984457539)
 
     val input = parseInput(readInput(day = DAY))
