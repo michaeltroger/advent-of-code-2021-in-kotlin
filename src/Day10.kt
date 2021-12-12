326a855ab0a343b1eb9faad5f8efb2e151d1e27a
Michael Troger <dev@michaeltroger.com>
2021-12-12 18:11:52 +0100

    Solve part 1 day 10

--- src/Day10.kt
+++ src/Day10.kt
@@ -2,7 +2,27 @@ private const val DAY = 10
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return -1
+        var syntaxErrorScore = 0
+        input.mapIndexed { lineNumber, line ->
+            val expectedCloseSigns = mutableListOf<Char>()
+            line.forEach { actualChar ->
+                when(actualChar) {
+                    '[' -> expectedCloseSigns.add(']')
+                    '(' -> expectedCloseSigns.add(')')
+                    '{' -> expectedCloseSigns.add('}')
+                    '<' -> expectedCloseSigns.add('>')
+                    ']', ')', '}', '>' -> {
+                        val expected = expectedCloseSigns.removeLast()
+                        if (expected != actualChar) {
+                            syntaxErrorScore += getSyntaxErrorPoints(actualChar)
+                            return@forEach
+                        }
+                    }
+                }
+            }
+        }
+
+        return syntaxErrorScore
     }
 
     fun part2(input: List<String>): Int {
@@ -11,10 +31,18 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(day = DAY, useTestInput = true)
-    check(part1(testInput) == 15)
-    check(part2(testInput) == 1134)
+    check(part1(testInput) == 26397)
+    //check(part2(testInput) == 1134)
 
     val input = readInput(day = DAY)
     println(part1(input))
     println(part2(input))
+}
+
+private fun getSyntaxErrorPoints(char: Char) = when (char) {
+    ')' -> 3
+    ']' -> 57
+    '}' -> 1197
+    '>' -> 25137
+    else -> throw Error("unexpected char")
 }
